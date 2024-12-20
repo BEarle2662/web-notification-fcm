334642e06dbee1c782ec87e3784e425e095a5ff7
BEarle2662 <bearle@gitam.edu>
2024-12-20 17:54:06 +0530

    satish name

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { generateToken, messaging } from "./notifications/firebase";
@@ -15,7 +15,7 @@ function App() {
         <div
           className={`${
             t.visible ? "animate-enter" : "animate-leave"
-          } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+          } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-white ring-opacity-5`}
         >
           <div className="flex-1 w-0 p-4">
             <div className="flex items-start">
@@ -48,10 +48,227 @@ function App() {
       ));
     });
   }, []);
+
+  const [name, setName] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value.toLowerCase());
+  };
   return (
-    <div className="App">
-      <h1>Engagement Day</h1>
+    <div className="p-4 min-h-screen flex flex-col justify-evenly bg-gradient-to-b from-[#200122]  to-[#6f0000]">
+      {/* s */}
+      {name == "s" && (
+        <div className="App flex justify-center">
+          <div className="  w-[90vw] h-[70vh] md:w-[20vw] md:h-[50vh] p-4">
+            <div className="grid grid-cols-12 grid-rows-5 h-full w-full  gap-2">
+              <div className="col-span-4 row-span-1 rounded-ss-xl border border-white ">
+                <img
+                  src="/assets/s1.jpeg"
+                  className="object-cover object-center  rounded-ss-xl  h-full w-full"
+                />
+              </div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-e-lg border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1 invisible  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-es-xl border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-se-xl border border-white"></div>
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1  border rounded-s-lg border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white"></div>
+              <div className="col-span-4 row-span-1 border rounded-ee-xl border-white"></div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* a */}
+      {name == "sa" && (
+        <div className="flex justify-center mt-5">
+          <div className="  w-[90vw] h-[70vh] md:w-[20vw] md:h-[50vh] p-4">
+            <div className="grid grid-cols-12 grid-rows-5 h-full w-full  gap-2">
+              <div className="col-span-4 row-span-1 rounded-ss-xl border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-se-xl border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1 invisible  border border-white"></div>
+
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white"></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1 rounded-b-xl  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+              <div className="col-span-4 row-span-1 rounded-b-xl border border-white"></div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* t */}
+      {name == "sat" && (
+        <div className="flex justify-center mt-5">
+          <div className="w-[90vw] h-[70vh] md:w-[20vw] md:h-[50vh] p-4">
+            <div className="grid grid-cols-12 grid-rows-5 h-full w-full  gap-2">
+              <div className="col-span-4 row-span-1 border rounded-s-xl border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white rounded-e-xl"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white "></div>
+              <div className="col-span-4 row-span-1   border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+              <div className="col-span-4 row-span-1 invisible  border border-white"></div>
+
+              <div className="col-span-4 row-span-1   border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white "></div>
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1  border border-white"></div>
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* i */}
+      {name == "sati" && (
+        <div className="flex justify-center mt-5">
+          <div className=" w-[90vw] h-[70vh] md:w-[20vw] md:h-[50vh] p-4">
+            <div className="grid grid-cols-12 grid-rows-5 h-full w-full  gap-2">
+              <div className="col-span-4 row-span-1 rounded-s-xl border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-e-xl border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white "></div>
+              <div className="col-span-4 row-span-1   border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+              <div className="col-span-4 row-span-1 invisible  border border-white"></div>
+
+              <div className="col-span-4 row-span-1   border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white "></div>
+              <div className="col-span-4 row-span-1 rounded-s-xl border border-white"></div>
+
+              <div className="col-span-4 row-span-1  border border-white"></div>
+              <div className="col-span-4 row-span-1 rounded-e-xl  border border-white"></div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* s */}
+
+      {name == "satis" && (
+        <div className="App flex justify-center">
+          <div className="  w-[90vw] h-[70vh] md:w-[20vw] md:h-[50vh] p-4">
+            <div className="grid grid-cols-12 grid-rows-5 h-full w-full  gap-2">
+              <div className="col-span-4 row-span-1 rounded-ss-xl border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-e-lg border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1 invisible  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-es-xl border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-se-xl border border-white"></div>
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1  border rounded-s-lg border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white"></div>
+              <div className="col-span-4 row-span-1 border rounded-ee-xl border-white"></div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* h */}
+      {name == "satish" && (
+        <div className="App flex justify-center">
+          <div className=" w-[90vw] h-[70vh] md:w-[20vw] md:h-[50vh] p-4">
+            <div className="grid grid-cols-12 grid-rows-5 h-full w-full  gap-2">
+              <div className="col-span-4 row-span-1 rounded-ss-xl border border-white "></div>
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 rounded-se-xl border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1 invisible  border border-white"></div>
+
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white"></div>
+              <div className="col-span-4 row-span-1  border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+
+              <div className="col-span-4 row-span-1 border border-white "></div>
+              <div className="col-span-4 row-span-1 rounded-es-xl   border border-white"></div>
+
+              <div className="col-span-4 row-span-1 invisible border border-white"></div>
+              <div className="col-span-4 row-span-1 rounded-ee-xl border border-white"></div>
+            </div>
+          </div>
+        </div>
+      )}
       <Toaster position="bottom-center" />
+
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={onChangeName}
+        className=" p-2  w-full rounded-full"
+        placeholder="Enter Satish"
+      />
     </div>
   );
 }
